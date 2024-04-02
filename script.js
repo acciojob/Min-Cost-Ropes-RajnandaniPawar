e79551260d0295831a3f66c8e39fad87005ec04e
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-04-02 21:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	Arrays.sort(arr);
+	let sum =0 ;
+	foreach(let t=0; t<arr.length; t++){
+		sum = arr[t] + arr[t+1];
+	}
+  return sum;
 }
 
 module.exports=mincost;
