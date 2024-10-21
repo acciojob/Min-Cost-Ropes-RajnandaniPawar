c883f7bcafe6317af175a664c459a42315647d50
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-10-21 21:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-	arr.sort((a,b) => a-b);
-		let arr2 = [];
-	// arr.sort((a,b) => a-b);
-	let len = arr.length;
-	while(len != 1){
-		let sum = arr[0]+arr[1];
-		arr.splice(0,2);
-		arr2.push(sum);
-		arr2.sort((a,b) => a-b);
-		len--
-	}
-	let res = 0;
-	for(let i= 0; i<arr2.length; i++){
-		res += arr2[i];
-	}
-	return res;
+    // Step 1: Create a min-heap using a priority queue (min-heap)
+    const minHeap = new MinPriorityQueue();
+    
+    // Insert all rope lengths into the min-heap
+    for (let i = 0; i < arr.length; i++) {
+        minHeap.enqueue(arr[i]);
+    }
+
+    let totalCost = 0;
+
+    // Step 2: Keep connecting ropes until only one rope is left
+    while (minHeap.size() > 1) {
+        // Extract the two smallest ropes
+        let first = minHeap.dequeue().element;
+        let second = minHeap.dequeue().element;
+
+        // The cost to connect these two ropes
+        let cost = first + second;
+
+        // Add this cost to the total cost
+        totalCost += cost;
+
+        // Push the new rope (connected length) back into the heap
+        minHeap.enqueue(cost);
+    }
+
+    // Return the total cost of connecting all the ropes
+    return totalCost;
 
 }
 
